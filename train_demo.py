1b604a525312a277dd7f105910e460ee48014ad4
huaqingxu <huaqingxu89@gmail.com>
2023-04-14 17:21:25 +0800

    up

---
+++ train_demo.py
@@ -0,0 +1,45 @@
+from Informer.utils.tools import dotdict
+from Informer.exp.exp_informer import Exp_Informer
+import torch
+import pandas as pd
+from args_demo import args
+
+
+"""
+def set_random_seed(seed, deterministic=False):
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+set_random_seed(0)
+"""
+
+Exp = Exp_Informer
+
+for ii in range(args.itr):
+    setting = '{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_at{}_fc{}_eb{}_dt{}_mx{}_{}_{}'.format(args.model, args.data, args.features, 
+                args.seq_len, args.label_len, args.pred_len,
+                args.d_model, args.n_heads, args.e_layers, args.d_layers, args.d_ff, args.attn, args.factor, args.embed, args.distil, args.mix, args.des, ii)
+
+    exp = Exp(args)
+    
+    print('>>>>>>> start training: {} >>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+    exp.train(setting)
+    
+    print('>>>>>>> testing: {} <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    exp.test(setting)
+
+    torch.cuda.empty_cache()
+
+
+
+
+
+
+
+
+
